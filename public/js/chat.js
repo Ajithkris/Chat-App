fb0838450a49a8b2ace87c4aedcc5b4fc00cbd9a
Ajith Kumar <vajithkumar766@gmail.com>
2022-01-05 15:55:13 +0530

    init commit

---
+++ public/js/chat.js
@@ -0,0 +1,80 @@
+const socket = io()     //Send events
+
+
+const $messageForm = document.querySelector("#message-form")
+const $messageFormInput = $messageForm.querySelector("input")
+const $messageFormButton = $messageForm.querySelector("button")
+const $messages = document.querySelector("#messages")
+
+// Template
+const messageTemplate = document.querySelector("#message-template").innerHTML
+const locationMessageTemplate = document.querySelector("#location-message-template").innerHTML
+
+socket.on("message",(message) =>{
+    console.log(message);
+    const html = Mustache.render(messageTemplate,{
+        message : message.text,
+        createdAt: moment(message.createdAt).format("h:mm a")
+    })
+    $messages.insertAdjacentHTML("beforeend", html)
+})
+
+socket.on("locationMessage", (url)=>{
+    console.log(url);
+    const html = Mustache.render(locationMessageTemplate,{
+        url
+    })
+    $messages.insertAdjacentHTML("beforeend", html)
+})
+
+$messageForm.addEventListener("submit",(e)=>{
+
+    e.preventDefault()
+
+    $messageFormButton.setAttribute("disabled","disabled")
+
+    // const message = document.querySelector("input").value  // another select method below (html input tag)
+    const message = e.target.elements.message.value
+
+    socket.emit("sendMessage", message,(error)=>{
+        $messageFormButton.removeAttribute("disabled")
+        $messageFormInput.value = ""
+        $messageForm.focus()
+
+        if(error){
+            return console.log(error);
+        }
+        console.log("Message delivered");
+    })
+})
+
+document.querySelector("#send-location").addEventListener("click",()=>{
+    if(!navigator.geolocation){
+        return alert("Geolocatin not supported by your Browser")
+    }
+
+    navigator.geolocation.getCurrentPosition((position)=>{
+        socket.emit("sendLocation",{
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        },() => {
+            console.log("Location shared!");
+        })
+    })
+})
+
+
+
+
+
+
+
+
+// socket.on("countUpdated",(count) => {
+//     console.log("The count has been updated!", count);
+// })   //Recive events
+
+// document.querySelector("#increment").addEventListener("click", ()=>{
+//     console.log("clicked!");
+//     socket.emit("increment")
+// })
---
+++ src/index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const path = require('path');
+const http = require("http");
+const socketio = require("socket.io");
+const Filter = require("bad-words");
+const { generateMessage } = require("./utils/messages");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server)
+
+const port = process.env.PORT || 3000
+const publicDirectoryPath = path.join(__dirname,"../public");
+
+app.use(express.static(publicDirectoryPath));
+
+let count = 0;
+
+// server (emit) -> client (receive) -- countUpdated
+// client (emit) -> server (receive) -- increment
+
+io.on("connection", (socket) => {
+    console.log("New Websocket connectin");
+
+    socket.emit("message",generateMessage("welcome!"))
+    socket.broadcast.emit("message",generateMessage("New user has joined!"))
+
+    socket.on("sendMessage",(message,callback)=>{
+        const filter = new Filter()
+
+        if(filter.isProfane(message)){
+            return callback("Profanity is not allowed!")
+        }
+
+        io.emit("message",generateMessage(message))
+        callback()
+    })
+
+    socket.on("sendLocation", (coords, callback) => {
+        io.emit("message", `http://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        callback()
+    })
+
+    socket.on("disconnect",()=>{
+        io.emit("message",generateMessage("A user has left!"))
+    })
+
+    // socket.emit("countUpdated",count)
+
+    // socket.on("increment", ()=>{
+    //     count++
+    //     // socket.emit("countUpdated", count)    ======>>> single user
+    //     io.emit("countUpdated", count)   // mult like group--chat
+    // })
+})
+
+server.listen(port,()=>{
+    console.log("server is up on port"+ port);
+})
